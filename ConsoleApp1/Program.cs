18c5f5ac8d1f712fe0496e85dbe998cf885d60fd
vsproko1@gmail.com <vsproko1@gmail.com>
2021-10-26 22:51:00 +0300

    1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,24 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Zadanie1();
+        }
+        public static void Zadanie1()
+        {
+            int[] a = new int[] { 0, 16, 1, 7, 22, 3 };
+            int[] b = new int[] { 0, 1, 3, 2 };
+            int ob = 0; //колличество одинаковых элементов
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = 0; j < b.Length; j++) // проверка каждого элемента
+                {
+                    if (a[i] == b[j]) // условия для каждого элемента
+                    {
+                        ob = ob + 1;
+                    }
+                }
+            }
+            Console.WriteLine(ob);
         }
     }
 }
